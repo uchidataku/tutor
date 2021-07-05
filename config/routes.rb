399a0a57fec78429ea0ace5045b5d689bd2662ad
uchidataku <bbb.rhcp99@gmail.com>
2021-07-06 01:56:40 +0900

    auth_controller_specを仮実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :auth do
+    namespace :v1 do
+      post :sign_in, to: 'auth#sign_in'
+      post :sign_up, to: 'auth#sign_up'
+    end
+  end
 end
