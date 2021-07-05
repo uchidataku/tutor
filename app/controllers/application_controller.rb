c847998c6732955ce0ef0c582b7363289a6f6de5
uchidataku <bbb.rhcp99@gmail.com>
2021-07-06 01:27:07 +0900

    JWT関連を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 # ApplicationController
 class ApplicationController < ActionController::API
+  before_action :authenticate_account!
+
+  attr_reader :current_account
+
+  def authenticate_account!
+    @current_jwt = /[Bb]earer (.*)/.match(request.headers[:Authorization] || request.headers[:authorization]).to_a[1]
+    fail Exceptions::UnauthorizedError, 'JWTが必要です' unless @current_jwt
+
+    @current_account = Account.authenticate!(@current_jwt)
+  end
 end
