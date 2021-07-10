e5e828092ff9861d11b3deb1b60a3f0aad6bf2de
uchidataku <bbb.rhcp99@gmail.com>
2021-07-11 01:18:35 +0900

    avatar使えるように

--- spec/factories/tutors.rb
+++ spec/factories/tutors.rb
@@ -12,5 +12,13 @@ FactoryBot.define do
     address { '東京都' }
 
     association :account
+
+    trait :with_avatar do
+      after(:build) do |tutor|
+        tutor.avatar.attach(io: File.open('spec/fixtures/files/sample.jpg'),
+                            filename: 'sample_image.jpg',
+                            content_type: 'image/jpeg')
+      end
+    end
   end
 end
