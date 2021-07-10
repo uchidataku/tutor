14d020dd8f7fa86e093639475ec9268721310462
uchidataku <bbb.rhcp99@gmail.com>
2021-07-11 01:00:46 +0900

    rubocop修正

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,7 +3,7 @@
 class Student < ApplicationRecord
   module CurrentClassification
     JUNIOR_HIGH_SCHOOL = 'junior_high_school' # 中学校
-    HIGH_SCHOOL = 'high_school' #高等学校
+    HIGH_SCHOOL = 'high_school' # 高等学校
     TECHNICAL_COLLEGE = 'technical_college' # 高等専門学校
   end
 
