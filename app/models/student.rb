bc11b119e1b2ad908c821f89f8c8570bd631a0a9
uchidataku <bbb.rhcp99@gmail.com>
2021-07-11 00:49:37 +0900

    avatar追加

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,10 +7,14 @@ class Student < ApplicationRecord
     TECHNICAL_COLLEGE = 'technical_college' # 高等専門学校
   end
 
+  # Associations
+  has_one_attached :avatar
   belongs_to :account
 
+  # Enum
   enum current_classification: { junior_high_school: 0, high_school: 1, technical_college: 2 }, _suffix: true
 
+  # Validation
   validates :username, presence: true, uniqueness: true
   validates :birthday, presence: true
 end
