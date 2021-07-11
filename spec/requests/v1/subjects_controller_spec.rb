2f5054b7b5b8526b666bca2f030df0f2943acd93
uchidataku <bbb.rhcp99@gmail.com>
2021-07-11 21:22:34 +0900

    テスト修正

--- spec/requests/v1/subjects_controller_spec.rb
+++ spec/requests/v1/subjects_controller_spec.rb
@@ -7,13 +7,14 @@ RSpec.describe V1::SubjectsController, type: :request do
 
   describe 'GET /v1/subjects' do
     subject(:request) { get v1_subjects_path, headers: headers }
+    let!(:subject_count) { Subject.count }
     let!(:subjects) { create_list(:subject, 3) }
 
     it '科目一覧が取得できること' do
       request
       expect(response).to have_http_status(:ok)
       parsed_body = JSON.parse(response.body)
-      expect(parsed_body.count).to eq 3
+      expect(parsed_body.count).to eq (subject_count + 3)
     end
   end
 end
