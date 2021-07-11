72229f6177f8050214d455e8017b3c0993af44dd
uchidataku <bbb.rhcp99@gmail.com>
2021-07-11 15:29:53 +0900

    subjects_controllerの実装

---
+++ spec/requests/v1/subjects_controller_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe V1::SubjectsController, type: :request do
+  let!(:account) { create(:account) }
+  let(:headers) { { Authorization: "Bearer #{account.jwt}" } }
+
+  describe 'GET /v1/subjects' do
+    subject(:request) { get v1_subjects_path, headers: headers }
+    let!(:subjects) { create_list(:subject, 3) }
+
+    it '科目一覧が取得できること' do
+      request
+      expect(response).to have_http_status(:ok)
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body.count).to eq 3
+    end
+  end
+end
