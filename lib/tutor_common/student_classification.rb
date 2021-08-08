db1e8d815ce008455e2ad57d0ae4f49a52cc7e51
uchidataku <bbb.rchp99@gmail.com>
2021-08-08 15:21:47 +0900

    rubocop修正

--- lib/tutor_common/student_classification.rb
+++ lib/tutor_common/student_classification.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
 module TutorCommon
   # 生徒の学位分類
   module StudentClassification
-    JUNIOR_HIGH_SCHOOL = 'junior_high_school' # 中学校
-    HIGH_SCHOOL = 'high_school' # 高等学校
-    TECHNICAL_COLLEGE = 'technical_college' # 高等専門学校
+    JUNIOR_HIGH_SCHOOL = 'junior_high_school'.freeze # 中学校
+    HIGH_SCHOOL = 'high_school'.freeze # 高等学校
+    TECHNICAL_COLLEGE = 'technical_college'.freeze # 高等専門学校
   end
 end
